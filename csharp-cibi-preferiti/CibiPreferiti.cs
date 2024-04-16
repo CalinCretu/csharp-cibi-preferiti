dbcefda3f68ef0255703a3605c6177cfd67b9b29
CalinCretu <calincretu1412@gmail.com>
2024-04-16 15:45:13 +0200

    Esercizio completato in un solo commit. Perdonatemelo per la prima esercitazione dato che molto rapida.

---
+++ csharp-cibi-preferiti/CibiPreferiti.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CibiPreferiti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                // Dichiaro l'Array di stringhe
+            string[] CibiPreferiti = ["Pasta", "Secondi", "Pesci", "Pizza", "Lasagne", "Sushi", "Agnellini", "Verdure", "Insalate", "Risotti"];
+
+            Console.WriteLine("La lunghezza della classifica è di " + CibiPreferiti.Length + " " + "posizioni");
+                // Aggiungo questo Console. per avere un risultato comprensibile in console
+            Console.WriteLine("La classifica dei Cibi preferiti è:");
+            
+            for (var i=0; i<CibiPreferiti.Length; i++)
+            {              
+                Console.WriteLine((i+1) + "º" + " " + CibiPreferiti[i]);
+            }
+
+            Console.WriteLine("Il cibo preferito è: " + CibiPreferiti[0]);
+                // Per prendere l'ultimo elemento dell'array bisogna prendere la sua lunghezza e diminuire di 1
+                // Per far stampare la stringa bisogna prendere l'array e richiedergli la posizione trovata come ultima
+            Console.WriteLine("Il cibo meno gradito è: " + CibiPreferiti[CibiPreferiti.Length - 1]);
+
+                // troviamo gli elementi al centro della classifica
+            int posizioneCentrale = CibiPreferiti.Length / 2;
+
+            Console.WriteLine("La Prima posizione centrale è: " + CibiPreferiti[posizioneCentrale - 1]);
+
+            Console.WriteLine("La Seconda posizione centrale è: " + CibiPreferiti[posizioneCentrale]);
+        }
+    }
+}
